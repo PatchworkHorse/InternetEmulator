49c362f2bf56197e634b4b8c7f5d96e576f84465
Patchwork Horse <paintcolt@gmail.com>
2025-09-29 22:22:42 -0400

    Start adding IX, add another router to ATG

--- scripts/entrypoint-router.sh
+++ scripts/entrypoint-router.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-echo "Starting configuration of virtual border router ${ISP_NAME} (AS${ASN})"
+case "$ROUTER_TYPE" in border|interior|route-server)
+        ;;
+    *)
+        echo "ERROR: Invalid ROUTER_TYPE '$ROUTER_TYPE'. Exiting."
+        exit 1
+        ;;
+esac
+
+echo "Configuring virtual ${ROUTER_TYPE} router ${HOSTNAME} for ${ISP_NAME} (AS${ASN})"
 
 # Replace container daemons file with our template
-cp /scripts/daemons-template-border /etc/frr/daemons
+cp /scripts/daemons-template-${ROUTER_TYPE} /etc/frr/daemons
 
 # Copy in our custom FRR config
 cp /scripts/frr-${HOSTNAME}.conf /etc/frr/frr.conf
-
-chown frr:frr /var/log/frr -R
 
 # Enable IP forwarding at the kernel level
 sysctl -w net.ipv4.ip_forward=1
